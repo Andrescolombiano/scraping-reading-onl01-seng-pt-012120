e6858bce2ce2903bfc9fa0ced343d205dd44da5d
Andres Carvajal <jc1993box@hotmail.com>
2020-03-13 04:17:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-puts doc.css(".headline-26OIBN")
-<h1 class="headline-26OIBN">Change things.</h1>
 
-
-html = open("https://flatironschool.com/")
-
-  doc = Nokogiri::HTML(html)
-
- 
- 
  
  html = open("https://flatironschool.com/")
 
